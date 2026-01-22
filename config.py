adbf02c1ea090a74af14cbf5988a7e509c5c35b2
DARKVENOM <mkansikhethoashmond@gmail.com>
2026-01-22 19:26:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID", "5725238764"))
 PORT = int(os.getenv("PORT", "8080"))
+
+PUBLIC_DOMAIN = os.getenv("RAILWAY_PUBLIC_DOMAIN")
