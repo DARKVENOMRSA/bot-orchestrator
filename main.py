d972e845e4dba07ffb3044134920e623cd380810
DARKVENOM <mkansikhethoashmond@gmail.com>
2026-01-22 16:29:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from core.db import init_db
 from services.control_bot import start_bot_panel
 
-init_db()
-print("System Ready")
-
-start_bot_panel()
+if __name__ == "__main__":
+    print("System Ready")
+    start_bot_panel()
