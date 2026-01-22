501d41ddab0d567cadf67dcc1df25b4c60f2d8de
DARKVENOM <mkansikhethoashmond@gmail.com>
2026-01-22 19:14:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID") or "0")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+OWNER_ID = int(os.getenv("OWNER_ID", "5725238764"))
+PORT = int(os.getenv("PORT", "8080"))
