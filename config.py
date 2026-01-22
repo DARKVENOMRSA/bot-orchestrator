003ecc892b81f627673818e5f7cf7007f0209592
DARKVENOM <mkansikhethoashmond@gmail.com>
2026-01-22 18:18:21 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN") or "8314307280:AAHG7ac05Mm9uHkVfBi1cwREFIbwxCJ2G_U"
+OWNER_ID = int(os.getenv("OWNER_ID") or "5725238764)
+
+MAX_BOTS = 10
+MAX_RAM_MB = 150
+BOT_FOLDER = "bots"
